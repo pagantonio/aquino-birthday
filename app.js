f3ce81401d613bcb8bfd26232b595bfa9359008c
Pedro Antonio <Pedro Antonio>
2017-04-19 21:01:27 -0300

    Made 'aquino.html' the default page

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+var options = {
+  index: '/static/aquino.html'
+};
+
 app.use('/static', express.static('static'));
 app.use('/js', express.static('js'));
-
-app.get('/', (request, response) => {
-  response.send('Hello from Express!')
-})
+app.get('/', function(req, res) {
+    res.redirect('/static/aquino.html');
+});
 
 app.listen(port, (err) => {
   if (err) {
